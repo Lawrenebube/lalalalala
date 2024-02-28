138dabed7afc0a1a95759fb67c0b23dfd8b6effe
unknown <lawrenciaebube@gmail.com>
2024-02-28 14:09:47 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        'outfit':['Outfit'],
+        'young': ['Young-serif']
+      },
+      colors : {
+        primary: {
+          'Nutmeg': 'hsl(14, 45%, 36%)',
+          'DarkRaspberry': 'hsl(332, 51%, 32%)'
+        },
+        Neutral: {
+          
+          'White': 'hsl(0, 0%, 100%)',
+          'RoseWhite': 'hsl(330, 100%, 98%)',
+          'Eggshell': 'hsl(30, 54%, 90%)',
+          'LightGrey': 'hsl(30, 18%, 87%)',
+          'WengeBrown': 'hsl(30, 10%, 34%)',
+          'DarkCharcoal':' hsl(24, 5%, 18%)'   
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
