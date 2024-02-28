f1b09efb695f428dd9c12604fe88307295925abb
unknown <lawrenciaebube@gmail.com>
2024-02-28 19:49:51 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./*.{html,js}"],
 
   theme: {
     extend: {
       fontFamily: {
-        'outfit':['Outfit'],
-        'young': ['Young-serif']
+        outfit: ["Outfit"],
+        young: ["Young-serif"],
       },
-      colors : {
+      colors: {
         primary: {
-          'Nutmeg': 'hsl(14, 45%, 36%)',
-          'DarkRaspberry': 'hsl(332, 51%, 32%)'
+          Nutmeg: "hsl(14, 45%, 36%)",
+          DarkRaspberry: "hsl(332, 51%, 32%)",
         },
         Neutral: {
-          
-          'White': 'hsl(0, 0%, 100%)',
-          'RoseWhite': 'hsl(330, 100%, 98%)',
-          'Eggshell': 'hsl(30, 54%, 90%)',
-          'LightGrey': 'hsl(30, 18%, 87%)',
-          'WengeBrown': 'hsl(30, 10%, 34%)',
-          'DarkCharcoal':' hsl(24, 5%, 18%)'   
-        }
-      }
+          White: "hsl(0, 0%, 100%)",
+          RoseWhite: "hsl(330, 100%, 98%)",
+          Eggshell: "hsl(30, 54%, 90%)",
+          LightGrey: "hsl(30, 18%, 87%)",
+          WengeBrown: "hsl(30, 10%, 34%)",
+          DarkCharcoal: " hsl(24, 5%, 18%)",
+        },
+      },
     },
   },
   plugins: [],
-}
-
+};
